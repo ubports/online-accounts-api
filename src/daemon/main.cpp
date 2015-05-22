fabe990e7173ec0ad0c93762a3ff4fa0a37087b6
Alberto Mardegan <alberto.mardegan@canonical.com>
2015-05-22 11:29:47 +0300

    Headers and D-Bus disconnection

--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -1,3 +1,23 @@
+/*
+ * This file is part of OnlineAccountsDaemon
+ *
+ * Copyright (C) 2015 Canonical Ltd.
+ *
+ * Contact: Alberto Mardegan <alberto.mardegan@canonical.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "manager.h"
 #include "manageradaptor.h"
 
@@ -12,7 +32,12 @@ int main(int argc, char **argv)
     new ManagerAdaptor(server);
 
     QDBusConnection bus = QDBusConnection::sessionBus();
-    bus.registerService("com.ubuntu.OnlineAccounts.Manager");
     bus.registerObject("/com/ubuntu/OnlineAccounts/Manager", server);
-    return app.exec();
+    bus.registerService("com.ubuntu.OnlineAccounts.Manager");
+
+    int ret = app.exec();
+
+    bus.unregisterService("com.ubuntu.OnlineAccounts.Manager");
+    bus.unregisterObject("/com/ubuntu/OnlineAccounts/Manager");
+    return ret;
 }
